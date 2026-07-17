7735066f00105ec8e306867a4bba490076850448
thetrueryan <georgytoper@gmail.com>
2026-07-17 20:07:04 +0400

    add csr exceptions handling & new csr regs

--- csrfile.v
+++ csrfile.v
@@ -5,8 +5,13 @@ module csrfile(
  input wire [11:0] ra, wa,
  input wire [31:0] wd,
 
+ input wire [31:0] mcause_in,
+ input wire [31:0] pc,
+
+ output wire [31:0] rd,
+
  output wire csr_exc,
- output wire [31:0] rd
+ output wire [31:0] mtvec_out
 );
 
 // MODES
@@ -15,7 +20,7 @@ parameter reg [1:0] SUPERVISOR = 2'b01;
 parameter reg [1:0] USER = 2'b00;
 
 // REGFILE
-reg [31:0] rf [29];
+reg [31:0] rf [64];
 
 // MACHINE MODE
 parameter reg [11:0] MISA = 12'h301;
@@ -30,6 +35,10 @@ parameter reg [11:0] MSTATUS = 12'h300;
 parameter reg [11:0] MSTATUSH = 12'h310;
 parameter reg [11:0] MTVEC = 12'h305;
 parameter reg [11:0] MCAUSE = 12'h342;
+parameter reg [11:0] MEPC   = 12'h341;
+// COUNTER REGS
+parameter reg [11:0] MCYCLE  = 12'hB00;
+parameter reg [11:0] MCYCLEH = 12'hB80;
 
 // USER MODE
 // FLOATING-POINT REGS
@@ -44,7 +53,6 @@ parameter reg [11:0] INSTRET = 12'hC02;
 parameter reg [11:0] CYCLEH = 12'hC80;
 parameter reg [11:0] TIMEH = 12'hC81;
 parameter reg [11:0] INSTRETH = 12'hC82;
-
 
 // SUPERVISOR MODE
 // STATE REGS
@@ -61,58 +69,71 @@ parameter reg [11:0] STVAL = 12'h143;
 parameter reg [11:0] SATP = 12'h180;
 
 
-function automatic [4:0] get_idx;
+function automatic [5:0] get_idx;
  input [11:0] addr;
  begin
   case (addr)
    // MACHINE MODE
-   MISA:        get_idx = 5'd0;
-   MVENDORID:   get_idx = 5'd1;
-   MARCHID:     get_idx = 5'd2;
-   MIMPID:      get_idx = 5'd3;
-   MHARTID:     get_idx = 5'd4;
-   MCONFIGPTR:  get_idx = 5'd5;
-   MSTATUS:     get_idx = 5'd6;
-   MSTATUSH:    get_idx = 5'd7;
-   MTVEC:       get_idx = 5'd8;
-   MCAUSE:      get_idx = 5'd9;
+   MISA:        get_idx = 6'd32;
+   MVENDORID:   get_idx = 6'd1;
+   MARCHID:     get_idx = 6'd2;
+   MIMPID:      get_idx = 6'd3;
+   MHARTID:     get_idx = 6'd4;
+   MCONFIGPTR:  get_idx = 6'd5;
+   MSTATUS:     get_idx = 6'd6;
+   MSTATUSH:    get_idx = 6'd7;
+   MTVEC:       get_idx = 6'd8;
+   MCAUSE:      get_idx = 6'd9;
+   MEPC:        get_idx = 6'd29;
+   MCYCLE:      get_idx = 6'd30;
+   MCYCLEH:     get_idx = 6'd31;
    // SUPERVISOR MODE
-   SSTATUS:     get_idx = 5'd19;
-   STVEC:       get_idx = 5'd20;
-   SIE:         get_idx = 5'd21;
-   SIP:         get_idx = 5'd22;
-   SCOUNTEREN:  get_idx = 5'd23;
-   SSCRATCH:    get_idx = 5'd24;
-   SEPC:        get_idx = 5'd25;
-   SCAUSE:      get_idx = 5'd26;
-   STVAL:       get_idx = 5'd27;
-   SATP:        get_idx = 5'd28;
+   SSTATUS:     get_idx = 6'd19;
+   STVEC:       get_idx = 6'd20;
+   SIE:         get_idx = 6'd21;
+   SIP:         get_idx = 6'd22;
+   SCOUNTEREN:  get_idx = 6'd23;
+   SSCRATCH:    get_idx = 6'd24;
+   SEPC:        get_idx = 6'd25;
+   SCAUSE:      get_idx = 6'd26;
+   STVAL:       get_idx = 6'd27;
+   SATP:        get_idx = 6'd28;
    // USER MODE
-   FFLAGS:      get_idx = 5'd10;
-   FRM:         get_idx = 5'd11;
-   FCSR:        get_idx = 5'd12;
-   CYCLE:       get_idx = 5'd13;
-   TIME:        get_idx = 5'd14;
-   INSTRET:     get_idx = 5'd15;
-   CYCLEH:      get_idx = 5'd16;
-   TIMEH:       get_idx = 5'd17;
-   INSTRETH:    get_idx = 5'd18;
+   FFLAGS:      get_idx = 6'd10;
+   FRM:         get_idx = 6'd11;
+   FCSR:        get_idx = 6'd12;
+   CYCLE:       get_idx = 6'd13;
+   TIME:        get_idx = 6'd14;
+   INSTRET:     get_idx = 6'd15;
+   CYCLEH:      get_idx = 6'd16;
+   TIMEH:       get_idx = 6'd17;
+   INSTRETH:    get_idx = 6'd18;
 
-   default:     get_idx = 5'd0;
+   // DEFAULT (ZERO FOR EXCEPTION)
+   default:     get_idx = 6'd0;
   endcase
  end
 endfunction
 
+
+assign csr_exc = (mode < ra[9:8])
+| (we & (mode < wa[9:8]))
+| (we & (wa[11:10] == 2'b11))
+| (we & !get_idx(wa))
+| (!get_idx(ra));
+
 always @(posedge clk) begin
- if (we) begin
-  if (wa[11:10] == 2'b11) begin
-   csr_exc <= 1;
-  end else begin
+ if (csr_exc) begin
+  rf[get_idx(MEPC)] <= pc;
+  rf[get_idx(MCAUSE)] <= mcause_in;
+ end else begin
+  if (we) begin
    rf[get_idx(wa)] <= wd;
   end
  end
 end
 
+assign mtvec_out = rf[get_idx(MTVEC)];
 assign rd = (ra != 0) ? rf[get_idx(ra)] : 0;
 
 endmodule
