6810126047f6a65e87683e04cb646ad6cdfcfd9e
thetrueryan <georgytoper@gmail.com>
2026-08-11 00:10:04 +0400

    connect datapath & control unit to main processor module

--- cpu/datapath.v
+++ cpu/datapath.v
@@ -1,13 +1,29 @@
 module datapath(
  input wire clk, rst,
  
- input wire reg_write,
- input wire [31:0] read_data,
+ input wire regwrite, jump, alusrc, pcsrc,
+ input wire [31:0] readdata,
  input wire [31:0] instr,
+ input wire [1:0] resultvsrc,
+ input wire [4:0] alucode,
+ input wire [2:0] immcode,
+
  output wire [31:0] pc,
- output wire [31:0] write_data
+ output wire [31:0] writedata,
+ output wire [31:0] aluout,
+ output wire zero
 );
 
+///
+// output [4:0] alucode,
+// output regwrite, 
+// output alusrc, 
+// output memwrite, 
+// output jump, 
+// output pcsrc,
+// output [1:0] resultvsrc,
+// output [2:0] immcode
+//
 wire [31:0] pc_next, pc_plus4;
 
 assign pc_plus4 = pc + 32'd4;
---
+++ cpu/rv32_main.v
@@ -0,0 +1,54 @@
+module rv32_main(
+ input wire clk, rst,
+ 
+ input wire [31:0] instr,
+ input wire [31:0] readdata,
+
+ output wire memwrite,
+ output wire [31:0] pc,
+ output wire [31:0] writedata,
+ output wire [31:0] aluout
+);
+
+wire regwrite, alusrc, jump, pcsrc, zero;
+
+wire [1:0] resultvsrc;
+wire [2:0] immcode;
+wire [4:0] alucode;
+
+
+control_unit cu (
+ .opcode (instr[6:0]),
+ .func7 (instr[31:25]),
+ .func3 (instr[14:12]),
+ .zero (zero),
+ .alucode (alucode),
+ .regwrite (regwrite),
+ .alusrc (alusrc),
+ .memwrite (memwrite),
+ .jump (jump),
+ .pcsrc (pcsrc),
+ .resultvsrc (resultvsrc),
+ .immcode (immcode)
+
+);
+
+datapath dp (
+ .clk (clk), 
+ .rst (rst),
+ .regwrite (regwrite),
+ .jump (jump),
+ .alusrc (alusrc),
+ .pcsrc (pcsrc),
+ .readdata (readdata), 
+ .instr (instr),
+ .resultvsrc (resultvsrc),
+ .alucode (alucode),
+ .immcode (immcode),
+ .pc (pc),
+ .writedata (writedata),
+ .aluout (aluout),
+ .zero (zero)
+);
+
+endmodule
